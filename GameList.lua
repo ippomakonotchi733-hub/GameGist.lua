3ee8eb2f79cc1133582f986edfdf7151ab794c98
ippomakonotchi733-hub <ippomakonotchi733@gmail.com>
2026-06-25 11:39:29 +0800

    Add GameList.lua with game execution details

---
+++ GameList.lua
@@ -0,0 +1,26 @@
+return {
+    {
+        Title   = "blank baseplate",
+        PlaceId = 1367561549,
+        Image   = "rbxthumb://type=Asset&id=1367561549&w=420&h=420",
+        Execute = [[loadstring(game:HttpGetAsync("https://github.com/richie0866/remote-spy/releases/latest/download/RemoteSpy.lua"))()]],
+    },
+    {
+        Title   = "Project Rp",
+        PlaceId = 80816510181597,
+        Image   = "rbxthumb://type=Asset&id=80816510181597&w=420&h=420",
+        Execute = [[loadstring(game:HttpGet("https://gist.githubusercontent.com/ippomakonotchi733-hub/ddfa45f8cb90542774ad7d6d31da9b8d/raw/8d787cf17cc500e9b4c1ed35ad51aa929932b516/AUto%2520aim"))()]],
+    },
+    {
+        Title   = "Launch a Wheel",
+        PlaceId = 18916922845,
+        Image   = "rbxthumb://type=Asset&id=18916922845&w=420&h=420",
+        Execute = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/edgeiy/infiniteyield/master/source"))()]],
+    },
+    {
+        Title   = "The Mimic",
+        PlaceId = 6243699076,
+        Image   = "rbxthumb://type=Asset&id=6243699076&w=420&h=420",
+        Execute = [[loadstring(game:HttpGet("https://pastefy.app/yNWkzBxg/raw"))()]],
+    },
+}
